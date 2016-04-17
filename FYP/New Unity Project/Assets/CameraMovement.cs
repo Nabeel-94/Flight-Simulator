ee740fa7112cde9185045a858d28930e8ded0679
Nabeel <nabeelyaseen_94@yahoo.com>
2016-04-17 21:10:40 +0500

    Flight Simulator Project Added Simulation characters added

---
+++ FYP/New Unity Project/Assets/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+    
+
+	public float minX = -45.0f;
+	public float maxX = 45.0f;
+	
+	public float minY = -45.0f;
+	public float maxY = 45.0f;
+
+	public float sensX = 15.0f;
+	public float sensY = 15.0f;
+	
+	float rotationY = 0.0f;
+	float rotationX = 180.0f;
+
+	void Update () {
+
+		//if (Input.GetMouseButton (0)) {
+			rotationX += Input.GetAxis ("Mouse X") * sensX * Time.deltaTime;
+        //  rotationX = Mathf.Clamp(rotationX, minX  , maxX);
+          rotationY += Input.GetAxis ("Mouse Y") * sensY * Time.deltaTime;
+			rotationY = Mathf.Clamp (rotationY, minY, maxY);
+			transform.localEulerAngles = new Vector3 (-rotationY, rotationX, 0);
+		//}
+	}
+}
